01a92982875cc636d9225397c7db2390a30bf7c3
Mads Byrdal Nielsen <31509859+mbyrdal@users.noreply.github.com>
2025-04-01 12:39:03 +0200

    Added table with grid

--- vite-proj-env-1-4-2025/src/App.vue
+++ vite-proj-env-1-4-2025/src/App.vue
@@ -1,30 +1,28 @@
+<!--
+An example of creating a reusable grid component and using it with external data.
+-->
+
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import DemoGrid from './Grid.vue'
+import { ref } from 'vue'
+
+const searchQuery = ref('')
+const gridColumns = ['name', 'power']
+const gridData = [
+  { name: 'Chuck Norris', power: Infinity },
+  { name: 'Bruce Lee', power: 9000 },
+  { name: 'Jackie Chan', power: 7000 },
+  { name: 'Jet Li', power: 8000 }
+]
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <form id="search">
+    Search <input name="query" v-model="searchQuery">
+  </form>
+  <DemoGrid
+    :data="gridData"
+    :columns="gridColumns"
+    :filter-key="searchQuery">
+  </DemoGrid>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
